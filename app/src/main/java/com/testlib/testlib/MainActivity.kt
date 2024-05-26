7ee908364b68d86472750debac657b5cc7681c80
umairshaikh <96243458+umairshaikhdastgyr@users.noreply.github.com>
2024-05-26 14:41:14 +0500

    add view

--- app/src/main/java/com/testlib/testlib/MainActivity.kt
+++ app/src/main/java/com/testlib/testlib/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.testlib.testlib
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.startActivity
+import com.testlib.mylibrary.FormActivity
 import com.testlib.testlib.ui.theme.TestLibTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,9 +39,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context  = LocalContext.current
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.clickable {
+
+            context.startActivity(Intent(context, FormActivity::class.java))
+
+        }
     )
 }
 
