37b55315a1bc8350bcdd7349f398c0affba08278
umairshaikh <96243458+umairshaikhdastgyr@users.noreply.github.com>
2024-06-10 16:21:55 +0500

    remove main app imp

--- app/src/main/java/sa/halalah/hala_now/HalaNowApplication.kt
+++ app/src/main/java/sa/halalah/hala_now/HalaNowApplication.kt
@@ -1,7 +1,7 @@
 package sa.halalah.hala_now
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+//import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+//@HiltAndroidApp
 class HalaNowApplication: Application() {}
--- hala_now_library/src/main/java/sa/halalah/hala_now_library/HalaNowApplication.kt
+++ hala_now_library/src/main/java/sa/halalah/hala_now_library/HalaNowApplication.kt
@@ -3,5 +3,5 @@ package sa.halalah.hala_now_library
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class HalaNowApplication{}
